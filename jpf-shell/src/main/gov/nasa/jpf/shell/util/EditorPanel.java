8cdfc0d546c7d75a971327b1da1a5d8bec07cc48
Brad <brad_booth@live.com>
2020-04-19 15:17:57 -0400

    original files

--- jpf-shell/src/main/gov/nasa/jpf/shell/util/EditorPanel.java
+++ jpf-shell/src/main/gov/nasa/jpf/shell/util/EditorPanel.java
@@ -209,6 +209,7 @@ public class EditorPanel extends ShellPanel {
 
 	/**
 	 * Force the properties document to update when caret position changes
+	 * 
 	 * @param e
 	 */
 	private void onCaretEvent(CaretEvent e) {
