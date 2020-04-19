ef6313ffb317f4b6c532a288bcfaa7b66fff73ea
Brad <brad_booth@live.com>
2020-04-19 15:14:00 -0400

    original files

--- jpf-shell/src/main/gov/nasa/jpf/shell/panels/PropertiesPanel.java
+++ jpf-shell/src/main/gov/nasa/jpf/shell/panels/PropertiesPanel.java
@@ -20,11 +20,13 @@ package gov.nasa.jpf.shell.panels;
 import javax.swing.text.*;
 import gov.nasa.jpf.shell.util.EditorPanel;
 import gov.nasa.jpf.shell.ShellManager;
-
+import java.awt.Color;
+import java.awt.Font;
 import java.io.File;
-
+import java.io.IOException;
 import javax.swing.Icon;
-import javax.swing.JLabel;
+import javax.swing.JEditorPane;
+import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
@@ -38,16 +40,14 @@ public class PropertiesPanel extends EditorPanel {
 	private static final String DEFAULT_TITLE = "Properties";
 	private static final String DEFAULT_TIP = "Edit the Application Properties";
 	private static final String MIME_TYPE = "text/jpfproperties";
-	protected JLabel newLabel = new JLabel("File: ", JLabel.RIGHT);
 
 	public PropertiesPanel() {
 		super(DEFAULT_TITLE, null, DEFAULT_TIP);
 
-		String fName = ShellManager.getManager().getConfig().getProperty("jpf.app");
+		label.setText("Properties: ");
 
-		initContents("JPF application properties", fName);
-
-		this.errorField.setText("");
+		String fname = ShellManager.getManager().getConfig().getProperty("jpf.app");
+		initContents("JPF application properties", fname);
 	}
 
 	protected PropertiesPanel(String title, Icon icon, String tip) {
@@ -56,10 +56,9 @@ public class PropertiesPanel extends EditorPanel {
 
 	protected EditorKit getEditorKit() {
 		return new StyledEditorKit() {
-
 			@Override
 			public Document createDefaultDocument() {
-				return new PropertyDocument(errorField);
+				return new PropertyDocument();
 			}
 
 			@Override
@@ -87,4 +86,120 @@ public class PropertiesPanel extends EditorPanel {
 	protected FileNameExtensionFilter getFileFilter() {
 		return new FileNameExtensionFilter("JPF application properties", "jpf");
 	}
+}
+
+/**
+ * A PropertyDocument is the document type used to represent a *.jpf file. it
+ * includes the following styles:
+ * <dl>
+ * <dt>key</dt>
+ * <dd>The style to be applied to a property name</dd>
+ * <dt>value</dt>
+ * <dd>The style to be applied to a property value</dd>
+ * <dt>comment</dt>
+ * <dd>The style applied to a comment in the file.</dd>
+ * <dt>malformed</dt>
+ * <dd>The style applied to a malformed line.</dd>
+ * <dt>assignment</dt>
+ * <dd>The style applied to everything else (the '=' sign)</dd>
+ * </dl>
+ */
+class PropertyDocument extends DefaultStyledDocument {
+
+	private Style key;
+	private Style value;
+	private Style comment;
+	private Style malformed;
+	private Style assignment;
+
+	public PropertyDocument() {
+		super();
+
+		key = addStyle("key", null);
+		key.addAttribute(StyleConstants.Foreground, Color.BLUE);
+
+		value = addStyle("value", null);
+		value.addAttribute(StyleConstants.Foreground, Color.darkGray);
+
+		comment = addStyle("comment", null);
+		comment.addAttribute(StyleConstants.Foreground, Color.GRAY);
+
+		malformed = addStyle("malformed", null);
+		malformed.addAttribute(StyleConstants.Foreground, Color.RED);
+
+		assignment = addStyle("assignment", null);
+		assignment.addAttribute(StyleConstants.Foreground, Color.BLACK);
+	}
+
+	@Override
+	public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+		super.insertString(offs, str, a);
+		updateLines(offs, str.length());
+
+	}
+
+	@Override
+	public void remove(int offset, int length) throws BadLocationException {
+		super.remove(offset, length);
+		updateLines(offset, 0);
+	}
+
+	/**
+	 * Checks a portion of a document's text to see what syntax highlighting should
+	 * be applied.
+	 * 
+	 * @param offset which position in the document to start looking
+	 * @param length if the length of text that need be checked.
+	 * @throws BadLocationException
+	 */
+	private void updateLines(int offset, int length) throws BadLocationException {
+
+		int startLine = getDefaultRootElement().getElementIndex(offset);
+		int endLine = getDefaultRootElement().getElementIndex(offset + length);
+
+		for (int line = startLine; line <= endLine; line++)
+			colorLine(line);
+
+	}
+
+	/**
+	 * Sets an entire line number to the correct color that it should be.
+	 * 
+	 * @param lineNum
+	 * @throws BadLocationException
+	 */
+	private void colorLine(int lineNum) throws BadLocationException {
+		int startOffset = getDefaultRootElement().getElement(lineNum).getStartOffset();
+		int endOffset = getDefaultRootElement().getElement(lineNum).getEndOffset() - 1;
+		String line = getText(startOffset, endOffset - startOffset);
+
+		// First check if this uses the "+=" assignment operator
+		int assignIndex = line.indexOf("+=");
+		int assignLength = 2;
+
+		if (assignIndex == -1) {// += was not found, its probably an '=' assignment
+			assignIndex = line.indexOf('=');
+			assignLength = 1;
+		}
+
+		if (assignIndex > -1) {// We have a key/value pair here
+			// style the key
+			setCharacterAttributes(startOffset, assignIndex, key, true);
+			// style the value
+			int valueOffset = startOffset + assignIndex + assignLength;
+			setCharacterAttributes(valueOffset, line.length() - (assignIndex + assignLength), value, true);
+
+			// Don't forget to style the '=' sign
+			setCharacterAttributes(startOffset + assignIndex, assignLength, assignment, true);
+
+		} else {// This line isn't a comment or an assignment, it's probably malformed
+			setCharacterAttributes(startOffset, endOffset - startOffset + 1, malformed, true);
+		}
+
+		// And now after all of that, check to see if this has a comment in it
+		int commentIndex = line.indexOf("#");
+		if (commentIndex != -1)
+			setCharacterAttributes(startOffset + commentIndex, endOffset - (startOffset + commentIndex), comment, true);
+	}
+
 }
